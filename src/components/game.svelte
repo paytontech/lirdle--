689511a0306f453117ca39e995f953f4e2342365
paytondev <81662879+paytontech@users.noreply.github.com>
2023-03-10 13:16:35 -0500

    init

---
+++ src/components/game.svelte
@@ -0,0 +1,39 @@
+<script>
+    let gameData = {
+        guessCount: 0,
+        guesses: [
+            /*
+            example:
+                {word:'WATER',letters:[
+                    {letter:'w','placement':0,'state':0 (0 being not at all correct, 1 being incorrect placement, 2 being corrent placement.)}
+                ]}
+            */
+        ],
+    };
+</script>
+
+{#each gameData.guesses as guess}
+    <div id={guess.word} class="prevGuess">
+        {#each guess.letters as letter}
+            {#if letter.state == 1}
+                <p class="letterPrev" style="color:rgb(209, 141, 31)">{letter.letter}</p>
+            {:else if letter.state == 2}
+                <p class="letterPrev" style="color:green">{letter.letter}</p>
+            {:else}
+                <p class="letterPrev">{letter.letter}</p>
+            {/if}
+        {/each}
+    </div>
+{/each}
+<style>
+    .prevGuess {
+        display: flex;
+        width: 100%;
+        justify-content: center;
+    }
+    .letterPrev {
+        border-color: gray;
+        border-style: solid;
+        padding: 10px;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Game from '../components/game.svelte'
+</script>
+<h1>Slirdle - Lirdle except it only lies sometimes.</h1>
+<Game/>
+
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,600;1,600&display=swap');
+    :root {
+        font-family: 'Poppins', sans-serif;
+        text-align: center;
+    }
+</style>
